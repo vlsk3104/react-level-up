d85a5aa1bc9081f5f63ba53594e5a499cf9f2e3b
vlsk3104 <vlsk3104@gmail.com>
2022-03-21 19:50:30 +0900

    レンダリング確認用のコーディング

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ChildArea } from "./ChildArea";
 
-function App() {
+export default function App() {
+  console.log('App');
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => {
+    setOpen(! open);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </div>
   );
-}
-
-export default App;
+};
